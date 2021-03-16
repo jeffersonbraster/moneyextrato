5160068a487c5eb9520eb155d06e308ef59d21f9
Jefferson <jeffersonbraster@gmail.com>
2021-03-16 14:32:08 -0300

    estrutura do formulario

--- src/components/ModalTransaction/index.tsx
+++ src/components/ModalTransaction/index.tsx
@@ -1,4 +1,5 @@
 import Modal from "react-modal";
+import { Container } from "./styles";
 
 type modalTransactionTypes = {
   isOpen: boolean;
@@ -10,8 +11,23 @@ export function ModalTransaction({
   onRequestClose,
 }: modalTransactionTypes) {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h2>Cadastrar nova transação</h2>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <Container>
+        <h2>Cadastrar nova transação</h2>
+
+        <input type="text" placeholder="Titulo" />
+
+        <input type="number" placeholder="Valor" />
+
+        <input type="text" placeholder="Categoria" />
+
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   );
 }
